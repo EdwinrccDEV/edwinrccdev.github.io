429b78d481eb9af65444d0038d7b30901d29e914
Edwin_rcc [DEV] <edwincastchi14@gmail.com>
2025-09-05 21:46:54 -0700

    Iniciar cancion

--- cAudio.js
+++ cAudio.js
@@ -1,19 +1,24 @@
-    const audio = document.getElementById('miAudio');
-    const playPauseBtn = document.getElementById('playPauseBtn');
-    const volumenSlider = document.getElementById('volumen');
+const audio = document.getElementById('miAudio');
+const playPauseBtn = document.getElementById('playPauseBtn');
+const volumenSlider = document.getElementById('volumen');
+const selector = document.getElementById('currentSong');
 
-    // Play / Pause
-    playPauseBtn.addEventListener('click', () => {
-      if (audio.paused) {
-        audio.play();
-        playPauseBtn.textContent = 'Pausar';
-      } else {
-        audio.pause();
-        playPauseBtn.textContent = 'Reproducir';
-      }
-    });
+playPauseBtn.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+    playPauseBtn.textContent = 'Pausar';
+  } else {
+    audio.pause();
+    playPauseBtn.textContent = 'Reproducir';
+  }
+});
 
-    // Control de volumen
-    volumenSlider.addEventListener('input', () => {
-      audio.volume = volumenSlider.value;
-    });
+volumenSlider.addEventListener('input', () => {
+  audio.volume = volumenSlider.value;
+});
+
+selector.addEventListener('change', () => {
+  audio.src = selector.value;
+  audio.play();
+  playPauseBtn.textContent = 'Pausar';
+});
