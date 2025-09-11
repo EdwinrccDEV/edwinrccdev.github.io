aaaed90bbe1104451015d450c492ba938feb9e38
Edwin_rcc [DEV] <edwincastchi14@gmail.com>
2025-09-11 12:42:36 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,4 @@
+alert("[[ENGLISH]]\nThis page is still in a beta state, it may be buggy or nonfunctional\n\n[[ESPAÑOL]]\nEsta pagina aun esta un estado beta, asi que puede estar bugeada o no funcional");
 
 function textType(texting, time, idele) {
     document.addEventListener("DOMContentLoaded", function() {
