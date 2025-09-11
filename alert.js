4d665baf251c20229b329489f91c1adc66f1cc53
Edwin_rcc [DEV] <edwincastchi14@gmail.com>
2025-09-11 12:42:08 -0700

    Delete alert.js

--- alert.js
+++
@@ -1,1 +0,0 @@
-alert("[[ENGLISH]]\nThis page is still in a beta state, it may be buggy or nonfunctional\n\n[[ESPAÑOL]]\nEsta pagina aun esta un estado beta, asi que puede estar bugeada o no funcional");
